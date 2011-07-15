4e7e15f515952dafee521c550d48d863e83ec479
Kai Dietrich <mail@cleeus.de>
2011-07-15 07:15:10 +0200

    add: basic template rendering support

--- weavy.py
+++ weavy.py
@@ -3,13 +3,41 @@
 import sys
 import os
 from os import path
+import shutil
+
+def log(string):
+    print string
 
 class WeavyError(Exception):
     pass
 
-class MicroTemplate:
-    pass
+class MicroTemplateEngine:
+    def __init__(self, template_dir):
+        self.template_dir = template_dir
+        self.tpl = {}
 
+    def load_all_templates(self):
+        self.__load_tpl('site', 'html')
+        self.__load_tpl('blog', 'html')
+        self.__load_tpl('post', 'html')
+        self.__load_tpl('page', 'html')
+
+    def __load_tpl(self, template_name, file_ending):
+        f = open('%s/_%s.%s' % (self.template_dir, template_name, file_ending), 'rt')
+        self.tpl[template_name] = f.read().decode('utf8')
+        f.close()
+    
+    def __render(self, template, data):
+        temp = self.tpl[template]
+        for key, value in data.items():
+            temp = temp.replace('{{%s}}' % key, value, 1)
+        return temp
+
+    def render_post(self, title, content):
+        return self.__render('post', {'title':title, 'content':content})
+    
+    def render_site(self, navigation, content):
+        return self.__render('site', {'navigation':navigation, 'content':content})
 
 class FolderLocator:
     def __init__(self):
@@ -54,12 +82,29 @@ class FolderLocator:
     def get_pages_dir(self):
         return self.pages_dir
      
+class BlogTransformation:
+    def __init__(self, in_dir, out_dir):
+        self.in_dir = in_dir
+        self.out_dir = out_dir
 
 
+def erase_dir_contents(pathname):
+    shutil.rmtree(pathname)
+    os.mkdir(pathname)
 
 def main():
     floc = FolderLocator()
-    print(floc.get_out_dir())
+   
+    out_dir = floc.get_out_dir()
+    log('cleaning output dir: %s' % out_dir)
+    erase_dir_contents(out_dir)
+    
+    log('loading templates...')
+    template_dir = floc.get_template_dir()
+    mte = MicroTemplateEngine(template_dir)
+    mte.load_all_templates()
+
+    print mte.render_site('navi', 'the content')
 
     return 0
 
